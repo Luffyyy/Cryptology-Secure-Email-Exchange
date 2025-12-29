4e4f56b6e151441172bc36ab2cb273d2b791ff6d
EliasFarah2 <eliasfarah776@gmail.com>
2025-12-29 23:07:53 +0200

    New Logic
    Added a small menu

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import secrets
 import chacha20 as ch20
 import elgamal as eg
 import rabin_signature as rs
+import shared as sh
+import json
 
-user_data = { 'alice': {}, 'bob': {} } # Dummy database
+user_data = {
+    'alice': {
+        'messages': []
+    },
+    'bob': {
+        'messages': []
+    }
+} # Dummy database
 
 def send_message(from_user, to_user, msg):
-    msg = f'\tFROM: {from_user}\n\tTO: {to_user}\n\tMessage: {msg}'
+    msg = f'\tFROM: {from_user}\n\tMessage: {msg}'
     # Alice (Sender, Creates a key for Chacha20 and sends an Email to Bob)
     
     ## Chacha20 Symmetric Algorithm
@@ -43,8 +52,57 @@ def receive_message(to_user, cipher_email, elgamal_c1, elgamal_c2, sign, rabin_n
     ## Verify signature
     if rs.verify(msg, sign, rabin_n):
         print(f'Signature: Verified\nGot message:\n{msg}')
+        user_data[to_user]['messages'].append(msg)
     else:
         print(f'Rejected message: Received Invalid Signature.')
 
 
-send_message('alice', 'bob', input('Enter a message to send to Bob: '))
+def check_password(username1, password1):
+    with open("data.json", "r") as f:
+        data = json.load(f)
+    for user in data["users"]:
+        if user["username"] == username1:
+            return user["password"] == password1
+    return False
+
+start_action = int(input("Welcome!\nPress 1 to Log in\nPress 2 to exist\n"))
+while start_action != 2:
+    username = input('Enter Username: ')
+    password = input('Enter your password: ')
+    if not check_password(username, str(sh.hash_message_hex(password))):
+        print("Username or password is incorrect.")
+        start_action = int(input("Do you wish to try again?\n1 for yes\n2 for no\n"))
+    else:
+        print(f'Welcome {username}!')
+        action = input('Click 1 to send a message\nClick 2 to read your messages\nClick 3 to log out\nClick 4 to exit\n')
+        while True:
+            if action == '1':
+                print("Here is your contact lists:\n")
+                counter = 0
+                for user in user_data.keys():
+                    if user != username:
+                        counter += 1
+                        print(f"{counter}. {user}")
+                print("\nPlease state who you want to send a message to:\n")
+                to_user1 = input()
+                while to_user1 not in user_data:
+                    to_user1 = input('User does not exist! please try again! \nEnter Username: ')
+                if to_user1 in user_data:
+                    send_message(username, to_user1, input('Enter a message to send: '))
+                    print("Message sent successfully")
+            elif action == '2':
+                if not user_data[username]:
+                    print("Your inbox is empty.")
+                else:
+                    print(f"Here are your messages for {username}:")
+                    for i, msg in enumerate(user_data[username]['messages'], start=1):
+                        print(f"{i}. {msg}")
+            elif action == '3':
+                print("Logged out successfully\n")
+                start_action = int(input("Welcome!\n Press 1 to Log in\n Press 2 to exist\n"))
+                break
+            elif action == '4':
+                start_action = 2
+                break
+            action = input('Click 1 to send a message\nClick 2 to read your messages\nClick 3 to log out\nClick 4 to exit\n')
+print("Good bye!")
--- shared.py
+++ shared.py
@@ -3,3 +3,5 @@ import hashlib
 # Simple SHA-256 hash to integer
 def hash_message(msg):
     return int(hashlib.sha256(msg.encode()).hexdigest(), 16)
+def hash_message_hex(msg):
+    return hashlib.sha256(msg.encode()).hexdigest()
