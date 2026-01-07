50cbf0c745aabba1df5b44d82f615d4590d37b22
Luffy <luffydafloffi@gmail.com>
2026-01-07 22:26:29 +0200

    Make chacha20 more standardized (single counter)

--- chacha20.py
+++ chacha20.py
@@ -25,7 +25,7 @@ def yield_chacha20_xor_stream(key, iv, position=0):
         raise TypeError
     if len(key) != 32:
         raise ValueError
-    if len(iv) != 8:
+    if len(iv) != 12:
         raise ValueError
 
     def rotate(v, c):
@@ -44,8 +44,9 @@ def yield_chacha20_xor_stream(key, iv, position=0):
     ctx = [0] * 16
     ctx[:4] = (1634760805, 857760878, 2036477234, 1797285236)
     ctx[4:12] = struct.unpack('<8L', key)
-    ctx[12] = ctx[13] = position
-    ctx[14:16] = struct.unpack('<LL', iv)
+    ctx[12] = position
+    ctx[13:16] = struct.unpack('<3L', iv)
+
     while 1:
         x = list(ctx)
         for i in range(10):
@@ -59,16 +60,15 @@ def yield_chacha20_xor_stream(key, iv, position=0):
             quarter_round(x, 3, 4,  9, 14)
         for c in struct.pack('<16L', *((x[i] + ctx[i]) & 0xffffffff for i in range(16))):
             yield c
+
         ctx[12] = (ctx[12] + 1) & 0xffffffff
-        if ctx[12] == 0:
-            ctx[13] = (ctx[13] + 1) & 0xffffffff
 
 def encrypt(data, key, iv=None, position=0):
     """Encrypt (or decrypt) with the ChaCha20 cipher."""
     if not isinstance(data, bytes):
         raise TypeError
     if iv is None:
-        iv = b'\0' * 8
+        iv = b'\0' * 12
     if isinstance(key, bytes):
         if not key:
             raise ValueError('Key is empty.')
